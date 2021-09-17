ec8231f9a4625f7be723c2a6f6d5e5943db57c08
dgruending <dgruending23@gmail.com>
2021-09-17 11:10:52 +0000

    Created stage2 of the project

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 import stage1
+import stage2
 
 # call subroutine for current stage
 if __name__ == "__main__":
     # As required for the tests
     pd.set_option('display.max_columns', 8)
-    stage1.main()
+    # stage1.main()
+    stage2.main()
---
+++ Data Analysis for Hospitals/task/stage2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Stage 2/5: Merge them
+# Requirements:
+# 1.    After all the libraries imports write the following line of code:
+#       pd.set_option('display.max_columns', 8)
+# 2.    Read 3 CSV files with datasets
+# 3.    Change the column names. All column names in the sports and prenatal tables must match the column names
+#       in the general table
+# 4.    Merge the data frames into one. Use the ignore_index = True parameter and the following order:
+#       general, prenatal, sports
+# 5.    Delete the Unnamed: 0 column
+# 6.    Print random 20 rows of the resulting data frame. For the reproducible output set random_state=30
+
+
+def main():
+    pass
