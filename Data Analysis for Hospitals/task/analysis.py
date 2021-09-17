d2dd8fda60aad65a2f47f8e3648e0d1c04378cf3
dgruending <dgruending23@gmail.com>
2021-09-17 09:54:27 +0000

    Added Module for stage1 and Methodhead for the first test

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -1,1 +1,5 @@
-import pandas as pd
+import stage1
+
+# call subroutine for current stage
+if __name__ == "__main__":
+    stage1.main()
---
+++ Data Analysis for Hospitals/task/stage1.py
@@ -0,0 +1,11 @@
+# Stage 1/5: Upload the data
+# Requirements:
+# 1.    After all the libraries imports write the following line of code:
+#       pd.set_option('display.max_columns', 8)
+#       It sets the number of columns, which pandas lets to display in the terminal.
+#       Unfortunately, this number differs occasionally and causes problems in the tests, so we need to fix the number.
+# 2.    Read 3 CSV files containing the datasets
+# 3.    Print the first 20 rows of each data frame. Use the following order: 'general', 'prenatal', 'sports'
+
+def main():
+    pass
