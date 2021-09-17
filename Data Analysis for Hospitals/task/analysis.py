8f80062a2e3addc3075960d441d14779e0086837
dgruending <dgruending23@gmail.com>
2021-09-17 10:13:21 +0000

    Implemented stage 1

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -1,5 +1,9 @@
+import pandas as pd
+
 import stage1
 
 # call subroutine for current stage
 if __name__ == "__main__":
+    # As required for the tests
+    pd.set_option('display.max_columns', 8)
     stage1.main()
--- Data Analysis for Hospitals/task/stage1.py
+++ Data Analysis for Hospitals/task/stage1.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 # Stage 1/5: Upload the data
 # Requirements:
 # 1.    After all the libraries imports write the following line of code:
@@ -7,5 +9,12 @@
 # 2.    Read 3 CSV files containing the datasets
 # 3.    Print the first 20 rows of each data frame. Use the following order: 'general', 'prenatal', 'sports'
 
+
 def main():
-    pass
+    # to match the example output, one should use index_col=0, but that doesn't pass the test
+    general_df = pd.read_csv("test/general.csv")
+    prenatal_df = pd.read_csv("test/prenatal.csv")
+    sport_df = pd.read_csv("test/sports.csv")
+    print(general_df.head(20))
+    print(prenatal_df.head(20))
+    print(sport_df.head(20))
