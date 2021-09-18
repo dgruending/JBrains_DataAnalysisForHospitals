08164557aad998944bfc5b0184ddba4af9e17bcb
dgruending <dgruending23@gmail.com>
2021-09-18 09:24:19 +0000

    Added stage 3

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 # import stage1
-import stage2
+# import stage2
+import stage3
 
 # call subroutine for current stage
 if __name__ == "__main__":
     # As required for the tests
     pd.set_option('display.max_columns', 8)
     # stage1.main()
-    stage2.main()
+    # stage2.main()
+    stage3.main()
